4de8b765a1a9324a488e2702963422d50696c444
Manuela Taborda <tabordam1910@gmail.com>
2026-04-13 20:36:08 -0500

    commit

--- src/main/java/com/bank/domain/services/LoanDomainService.java
+++ src/main/java/com/bank/domain/services/LoanDomainService.java
@@ -5,18 +5,23 @@ import com.bank.domain.entities.Account;
 import com.bank.domain.exceptions.BusinessRuleException;
 
 public class LoanDomainService {
+
     public void approveLoan(Loan loan) {
+        // Rule: Only loans "Under Review" can be approved [cite: 121]
         if (!"Under Review".equalsIgnoreCase(loan.getLoanStatus())) {
             throw new BusinessRuleException("Only loans under review can be approved.");
         }
         loan.setLoanStatus("Approved");
     }
 
-    public void processDisbursement(Loan loan, Account targetAccount) {
+    public void processDisbursement(Loan loan, Account destinationAccount) {
+        // Rule: Disbursement only possible from "Approved" state [cite: 123]
         if (!"Approved".equalsIgnoreCase(loan.getLoanStatus())) {
             throw new BusinessRuleException("Loan must be approved before disbursement.");
         }
-        targetAccount.setBalance(targetAccount.getBalance() + loan.getApprovedAmount());
+        
+        // Rule: Target account must be active and increase balance [cite: 82]
+        destinationAccount.setBalance(destinationAccount.getBalance() + loan.getApprovedAmount());
         loan.setLoanStatus("Disbursed");
     }
 }
--- src/main/java/com/bank/domain/services/TransferDomainService.java
+++ src/main/java/com/bank/domain/services/TransferDomainService.java
@@ -5,20 +5,25 @@ import com.bank.domain.entities.Account;
 import com.bank.domain.exceptions.BusinessRuleException;
 
 public class TransferDomainService {
-    private static final double CORPORATE_THRESHOLD = 10000.0;
 
-    public void processTransfer(Transfer transfer, Account source, Account destination) {
-        if (transfer.getAmount() > CORPORATE_THRESHOLD) {
+    private static final double CORPORATE_LIMIT = 10000.0; // Business Rule Threshold [cite: 85]
+
+    public void initiateTransfer(Transfer transfer, Account source) {
+        // Rule: If amount exceeds threshold, set to "Pending Approval" [cite: 87]
+        if (transfer.getAmount() > CORPORATE_LIMIT) {
             transfer.setTransferStatus("Pending Approval");
-            return;
+        } else {
+            executeFundsMovement(transfer, source, null);
         }
-        executeMovement(transfer, source, destination);
     }
 
-    public void executeMovement(Transfer transfer, Account source, Account destination) {
+    public void executeFundsMovement(Transfer transfer, Account source, Account destination) {
+        // Rule: Validate sufficient balance [cite: 91]
         if (source.getBalance() < transfer.getAmount()) {
             throw new BusinessRuleException("Insufficient funds.");
         }
+
+        // Rule: Update balances [cite: 124, 125]
         source.setBalance(source.getBalance() - transfer.getAmount());
         if (destination != null) {
             destination.setBalance(destination.getBalance() + transfer.getAmount());
