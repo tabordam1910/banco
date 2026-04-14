64d05d3ca798cb4bf638b4027445bc2e3a07fcfc
Manuela Taborda <tabordam1910@gmail.com>
2026-04-13 20:34:59 -0500

    commit

--- src/main/java/com/bank/domain/services/AccountDomainService.java
+++ src/main/java/com/bank/domain/services/AccountDomainService.java
@@ -5,15 +5,18 @@ import com.bank.domain.entities.User;
 import com.bank.domain.exceptions.BusinessRuleException;
 
 public class AccountDomainService {
-    public void validateNewAccount(User user) {
+
+    public void validateAccountOpening(User user) {
+        // Rule: Cannot open account if user is Inactive or Blocked [cite: 116]
         if ("Inactive".equalsIgnoreCase(user.getUserStatus()) || "Blocked".equalsIgnoreCase(user.getUserStatus())) {
             throw new BusinessRuleException("Cannot open account for inactive or blocked users.");
         }
     }
 
-    public void checkTransactionEligibility(Account account) {
+    public void verifyAccountOperational(Account account) {
+        // Rule: No operations allowed on Blocked or Cancelled accounts [cite: 119]
         if ("Blocked".equalsIgnoreCase(account.getAccountStatus()) || "Cancelled".equalsIgnoreCase(account.getAccountStatus())) {
-            throw new BusinessRuleException("Account is not eligible for transactions.");
+            throw new BusinessRuleException("Account is not active for transactions.");
         }
     }
 }
